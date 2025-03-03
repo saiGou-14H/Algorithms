ac50463c7d83811402539e32aed515924a2cfe00
saigou <70852096@qq.com>
2025-03-04 02:54:10 +0800

    2025/3/4 实现分析数据回传

--- service/__init__.py
+++ service/__init__.py
@@ -3,6 +3,7 @@ import torch
 from ultralytics import YOLO
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# face = YOLO("service/yolov8-pose-p6-HGStem-LSCD-BIFPN-Rep1.yaml").load("service/face.pt").to(device)
 face = YOLO("service/face.pt").to(device)
-expression = YOLO("service/bq-pose_best.pt").to(device)
+expression = YOLO("service/bq-best.pt").to(device)
 pose = YOLO("service/yolov8n-pose.pt").to(device)
